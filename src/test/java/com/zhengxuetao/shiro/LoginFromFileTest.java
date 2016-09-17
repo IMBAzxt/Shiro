1d71ea524754e59ee2ac237415786516b050ddff
zhengxuetao <zhengxuetao@haohandata.com.cn>
2016-09-17 20:25:42 +0800

    2016/09/17 shiro测试时使用maven会遇到的坑  by zhengxt 1、shiro在maven中构建时使用的是单线程，所以多个测试样例使用不同配置文件时，使用完后必须释放线程，否则报错

--- src/test/java/com/zhengxuetao/shiro/LoginFromFileTest.java
+++ src/test/java/com/zhengxuetao/shiro/LoginFromFileTest.java
@@ -6,6 +6,7 @@
 package com.zhengxuetao.shiro;
 
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.util.ThreadContext;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -34,7 +35,9 @@ public class LoginFromFileTest {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
+        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+        //SecurityUtils.getSubject().logout();
     }
 
     /**
